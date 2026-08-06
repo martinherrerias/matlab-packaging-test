fa6793f2029f06204a98aec6a728a18c9174323f
Martín Herrerías Azcué <martin.herrerias@gmail.com>
2026-08-06 18:50:21 +0100

    wip: build using ToolboxOptions

---
+++ release/buildfile.m
@@ -0,0 +1,20 @@
+function plan = buildfile
+import matlab.buildtool.tasks.*
+
+plan = buildplan(localfunctions);
+
+plan("clean") = CleanTask;
+plan("check") = CodeIssuesTask;
+plan("test") = TestTask;
+plan("release").Dependencies = ["check" "test"];
+
+plan.DefaultTasks = ["check" "test"];
+end
+
+function releaseTask(~)
+% Package Toolbox using matlab-toml
+
+    disp('Running release task')
+    opts = mapToolboxOptions();
+    matlab.addons.toolbox.packageToolbox(opts);
+end
---
+++ release/mapToolboxOptions.m
@@ -0,0 +1,114 @@
+function opts = mapToolboxOptions(prjTOML)
+% Populate matlab.addons.toolbox.ToolboxOptions from matlab.toml
+
+arguments
+    prjTOML (1,1) string {mustBeFile} = prjPath('matlab.toml')
+end
+
+wd = cd(fileparts(prjTOML));
+restoreWd = onCleanup(@() cd(wd));
+
+prj = readTOML(prjTOML);
+pkg = prj.package;
+
+root = fullfile(fileparts(prjTOML), pkg.package_root);
+pid = pkg.id;
+paths = {prj.folders.path, prj.folders.test};
+opts = matlab.addons.toolbox.ToolboxOptions(root, pid, ToolboxMatlabPath=paths);
+
+opts.ToolboxName = pkg.name;
+opts.ToolboxVersion = pkg.version;
+opts.AuthorName = pkg.provider.name;
+opts.AuthorCompany = pkg.provider.organization;
+opts.AuthorEmail = pkg.provider.email;
+opts.Summary = pkg.summary;
+opts.Description = pkg.description;
+
+opts = mapReleases(pkg.release_compatibility, opts);
+
+% TODO: TOML doesn't record "MATLAB online" compatibility
+opts.SupportedPlatforms = mapSupportedPlatforms(pkg.supported_platforms);
+
+opts.ToolboxImageFile = pkg.preview_image_file;
+opts.ToolboxGettingStartedGuide = pkg.getting_started_file;
+opts.OutputFile = [pkg.name '@' pkg.version '.mltbx'];
+
+% TODO: TOML doesn't record MATLAB add-ons
+% opts.RequiredAddons = ?
+
+% TODO: ToolboxOptions doesn't support non-scalar platforms
+% opts.RequiredAdditionalSoftware = f(pkg.required_additional_software);
+
+% % TODO: pointers to example/tutorial files
+% opts.AppGalleryFiles: [0×0 string]
+
+end
+
+function path = prjPath(varargin)
+    try
+        [status, root] = system('git rev-parse --show-toplevel');
+        assert(status == 0);
+        root = strtrim(root);
+    catch err
+        root = fileparts(fileparts(mfilename('fullpath')));
+        if isempty(root), root = pwd; end
+    end
+    path = fullfile(root, varargin{:});
+end
+
+function cfg = readTOML(prjTOML)
+    cfg = toml.read(prjTOML);
+    cfg = toml.map_to_struct(cfg);
+    cfg = flattenCells(cfg);
+end
+
+function s = flattenCells(s)
+% toml.map_to_struct likes returning fields wrapped as single cells
+
+    for fld = fieldnames(s)'
+        f = fld{1};
+        v = s.(f);
+        if iscell(v) && isscalar(v)
+            v = v{1};
+        end
+        if isstruct(v)
+            v = flattenCells(v);
+        end
+        s.(f) = v;
+    end
+end
+
+function opts = mapReleases(release_compatibility, opts)
+% Map '22.1.* - 26.2.*' to ['R2022a', 'R2026b']
+
+    pat = '(\d\d).([1,2])(?:\.\*)?\s?-\s?(\d\d)\.([1,2])(?:\.\*)?';
+    tokens = regexp(release_compatibility, pat, 'tokens');
+    if isempty(tokens)
+        warning('Failed to map release_compatibility %s', release_compatibility);
+    end
+    label = @(tk1,tk2) sprintf('R20%s%s',tk1, char('a' + strfind('12', tk2) - 1));
+    a = label(tokens{1}{1:2});
+    b = label(tokens{1}{3:4});
+
+    try
+        opts.MinimumMatlabRelease = a;
+    catch
+        opts.MinimumMatlabRelease = 'R2014b';
+    end
+       
+    try
+        opts.MaximumMatlabRelease = b;
+    catch
+        opts.MaximumMatlabRelease = '';
+    end
+end
+
+function s = mapSupportedPlatforms(supported_platforms)
+    map = struct(Win64='windows', Glnxa64='linux', MatlabOnline='online');
+    if year(datetime(version('-date'))) <= 2025
+        map.Maci64='macos';
+    else
+        map.Mac='macos';
+    end
+    s = structfun(@(x) ismember(x, supported_platforms), map, 'unif', false);
+end
