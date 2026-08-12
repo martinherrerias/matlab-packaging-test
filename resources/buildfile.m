7d386fdd02559524096e5db58b1c4be32ddf3dec
Martin HA <martin.herrerias@gmail.com>
2026-08-12 18:43:18 +0100

    Revert "Outsource build functionality (removes matlab-toml submodule)"
    This reverts commit 383d61637f898a81a671c3ace49df8023864cf19.

--- resources/buildfile.m
+++ resources/buildfile.m
@@ -15,6 +15,6 @@ function releaseTask(~)
 % Package Toolbox using matlab-toml
 
     disp('Running release task')
-    tomlFile = fullfile(fileparts(mfilename('fullpath')), '..', 'matlab.toml');
-    PkgBuild.fromTOML(tomlFile);
+    opts = mapToolboxOptions();
+    matlab.addons.toolbox.packageToolbox(opts);
 end
